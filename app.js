6095c61c9da646c663ba46159e64d91d26e48acb
PitaniHamsaraj <33552374+hamsarajpitani@users.noreply.github.com>
2021-01-26 14:45:43 +0530

    Add files via upload
    added JS in myfunction() > else statement 
    body.style.overflow = "hidden";
    body.style.overflow = "auto";
    
    changes to single statement
    body.classList.toggle("navbar-scroll-block");

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+let body = document.querySelector('html');
+
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
-
+    body.style.overflow = "hidden";
+    console.log(body);
     x.style.width = "100%";
     console.log(x);
     if (x.className === "navbar") {
       x.className += " responsive";
     } else {
+        body.style.overflow = "auto";
         x.className = "navbar";
     }
     console.log(x);
