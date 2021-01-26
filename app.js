2fad0e45ab54a7dd87db5fa8eac159c1331867cb
PitaniHamsaraj <33552374+hamsarajpitani@users.noreply.github.com>
2021-01-26 14:56:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ let body = document.querySelector('html');
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    body.style.overflow = "hidden";
+    
+     // body.style.overflow = "hidden";
+     //** 1st time click add 2nd click remove so on */
+    body.classList.toggle("navbar-scroll-block");
     console.log(body);
     x.style.width = "100%";
     console.log(x);
     if (x.className === "navbar") {
       x.className += " responsive";
     } else {
-        body.style.overflow = "auto";
+       // body.style.overflow = "auto";
         x.className = "navbar";
     }
     console.log(x);
